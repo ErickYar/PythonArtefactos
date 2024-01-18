14527b2d48285e927ebd25a42ed4829ded0475ce
ErickYar <erickyb1985@gmail.com>
2024-01-18 11:40:58 -0500

    web de articulos en una tienda

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    producto=models.CharField(max_length=100)
+    fecha=models.DateField()
+    categoria=models.CharField(max_length=1)
+    precio=models.DecimalField(max_digits=6,decimal_places=2)
+    stock=models.IntegerField()
